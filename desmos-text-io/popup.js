41be655a3960056432367b2bd8a08c940275db21
hyrodium <hyrodium@gmail.com>
2022-08-06 22:16:10 +0900

    add css, update appearance

--- desmos-text-io/popup.js
+++ desmos-text-io/popup.js
@@ -1,14 +1,14 @@
 document.getElementById('btn_export').addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, { message: 'export' }, (content) => {
-            document.getElementById('title').value = content;
+            document.getElementById('script_area').value = content;
         });
     });
 });
 
 document.getElementById('btn_import').addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        state_json = document.getElementById('title').value;
+        state_json = document.getElementById('script_area').value;
         chrome.tabs.sendMessage(tabs[0].id, { message: 'import', state_json: state_json }, () => { });
     });
 });
